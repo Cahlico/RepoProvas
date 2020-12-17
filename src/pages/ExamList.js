a26c67a5a8f0a299e25d0d8c61885a86b5493d1e
Carlos <carlosaugusto19991@poli.ufrj.br>
2020-12-17 13:55:19 -0300

    adding exams list page

--- src/pages/ExamList.js
+++ src/pages/ExamList.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 
-import { Container } from '../styles/styledExamList';
+import { Container, Title } from '../styles/styledExamList';
 
 export default function ExamList() {
+    const { state } = useLocation();
+    const [exams, setExams] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        let request;
+
+        if(typeof state === 'number') {
+            request = axios.get(`http://localhost:3000/api/v1/exams/professor:${state}`);
+        } else {
+            request = axios.get(`http://localhost:3000/api/v1/exams/subject:${state}`);
+        }
+
+        request.then(resp => {
+            setExams(resp.data);
+            setLoading(false);
+        });
+        request.catch(() => alert('ocorreu um erro no carregamento da página'));
+    }, []);
+
     return (
         <Container>
-        
-        
-        
-        
+            {!loading
+                ? exams.length !== 0
+                    ? <>
+                        <Title>{exams[0].name}</Title>
+                        {exams.map(e => (
+                            <div key={e.link}>
+                                <a href={e.link} target="_blank">Link para a prova</a>
+                                <p>{e.examType}</p>
+                            </div>
+                        ))}
+                    </>
+                    : <strong>A matéria ou professor ainda não contem nenhuma prova postada</strong>
+                : <strong>Carregando conteudo...</strong>
+            }  
         </Container>
     );
 }
--- src/styles/styledExamList.js
+++ src/styles/styledExamList.js
@@ -1,5 +1,24 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    
+    margin: 60px 10% 0 10%;
+
+    div {
+        display: flex;
+        margin: 20px 0;
+    }
+
+    a {
+        cursor: pointer;
+        color: #dcddde;
+    }
+
+    p {
+        margin-left: 20px;
+    }
+`;
+
+export const Title = styled.h1`
+    font-size: 28px;
+    font-weight: 900;
 `;
